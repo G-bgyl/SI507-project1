88b4ee243945d7876802a653a3cb68410f56c328
G-bgyl <gelinyan0801@foxmail.com>
2018-02-03 00:07:00 -0500

    finish the part 1 and part of part2

--- proj1_w18.py
+++ proj1_w18.py
@@ -1,12 +1,84 @@
+import json
 
 class Media:
 
-	def __init__(self, title="No Title", author="No Author"):
-		self.title = title
-		self.author = author
+    def __init__(self, title="No Title", author="No Author",release_year=1999,json = "sample_json.txt"):
+        self.title = title
+        self.author = author
+        self.release_year = release_year
+        self.json= json
+        self.data_dict = self.media_data()
+    def media_data(self):
+        media_data = []
+        with open(self.json) as inputData:
+            for line in inputData:
+                try:
+                    media_data.append(json.loads(line.rstrip('\n')))
+                except ValueError:
+                    print("Skipping invalid line {0}".format(repr(line)))
 
-## Other classes, functions, etc. should go here
+        for each in media_data:
+            if 'trackName' in each:
+                self.title = each['trackName']
+            if 'artistName' in each:
+                self.author = each['artistName']
+            if 'artistName' in each:
+                self.release_year = each['releaseDate'][0:4]
+            # print(self.title,self.author,self.release_year)
+
+
+    def __str__(self):
+        string_of_media = '{} by {} ({})'.format(self.title,self.author,self.release_year)
+        return string_of_media
+
+    def __len__(self):
+        return 0
+
+class Song(Media):
+    def __init__(self,title="No Title", author="No Author",release_year=1999,
+                 album='No Album', track_length=0,genre = 'No Genre'):
+        super().__init__(title, author, release_year)
+        self.album = album
+        self.track_length = track_length
+        self.genre = genre
+
+    def song_data(self):
+        for each in self.data_dict:
+            if 'collectionName' in each:
+                self.album = each['collectionName']
+            if 'trackTimeMillis' in each:
+                self.track_length = each['trackTimeMillis']
+            if 'primaryGenreName' in each:
+                self.genre = each['primaryGenreName']
+    def __str__(self):
+        string_of_media = super().__str__()+'[{}].'.format(self.genre)
+        return string_of_media
+    def __len__(self):
+        # return track length in seconds
+        length = round(self.track_length/1000)
+        return self.track_length
+
+class Movie(Media):
+    def __init__(self,title="No Title", author="No Author",
+                 release_year=1999,rating = 'No Rating',movie_length=0):
+        super().__init__(title, author, release_year)
+        self.rating = rating
+        self.movie_length = movie_length
+
+    def movie_data(self):
+        for each in self.data_dict:
+            if 'contentAdvisoryRating' in each:
+                self.rating = each['contentAdvisoryRating']
+            if 'trackTimeMillis' in each:
+                self.movie_length = each['trackTimeMillis']
+    def __str__(self):
+        string_of_media = super().__str__() + '[{}].'.format(self.rating)
+        return string_of_media
+    def __len__(self):
+        '''return movie length in minutes (rounded to nearest minute)'''
+        length = round(self.movie_length/60000)
+        return self.movie_length
 
 if __name__ == "__main__":
-	# your control code for Part 4 (interactive search) should go here
-	pass
+    # your control code for Part 4 (interactive search) should go here
+    media = Media()
--- proj1_w18_test.py
+++ proj1_w18_test.py
@@ -3,14 +3,45 @@ import proj1_w18 as proj1
 
 class TestMedia(unittest.TestCase):
 
-    def testConstructor(self):
+    def testMedia(self):
         m1 = proj1.Media()
-        m2 = proj1.Media("1999", "Prince")
+        m2 = proj1.Media("1999", "Prince", 1980)
 
         self.assertEqual(m1.title, "No Title")
         self.assertEqual(m1.author, "No Author")
         self.assertEqual(m2.title, "1999")
-        self.assertEqual(m2.author, "Prince")
+        self.assertEqual(m2.author, "Prince")#__str__
+        self.assertEqual(m2.__str__(), "1999 by Prince (1980)")  #
+        self.assertEqual(m2.__len__(), 0)
+
+        self.assertRaises(AttributeError, lambda: m1.rating)
+
+    def testSong(self):
+        s1=proj1.Song(title="Just a Dream", author="Sam Tsui",
+                      release_year=2011, album='Just A Dream',
+                      track_length=120, genre="Hip hop")
+        self.assertEqual(s1.title, "Just a Dream")
+        self.assertEqual(s1.author, "Sam Tsui")
+        self.assertEqual(s1.genre, "Hip hop")
+        self.assertEqual(s1.release_year, 2011)
+        self.assertEqual(s1.track_length, 120)
+        self.assertEqual(s1.__str__(), "Just a Dream by Sam Tsui (2011)[Hip hop].")  #
+        self.assertEqual(s1.__len__(), 120)
+        self.assertRaises(AttributeError,lambda :s1.rating)
 
 
-unittest.main()
+    def testMovie(self):
+        mo1 = proj1.Movie(title="Lady Bird", author="Greta Gerwig",
+                 release_year=2017, rating='R', movie_length=93)
+        self.assertEqual(mo1.title,"Lady Bird")
+        self.assertEqual(mo1.author, "Greta Gerwig")
+        self.assertEqual(mo1.release_year, 2017)
+        self.assertEqual(mo1.rating, 'R')
+        self.assertEqual(mo1.__str__(), "Lady Bird by Greta Gerwig (2017)[R].")  #
+        self.assertEqual(mo1.__len__(), 93)
+
+
+## The following is a line to run all of the tests you include:
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
+## verbosity 2 to see detail about the tests the code fails/passes/etc.
